5f141e1fb7dd27850ae3cf776a9923c1ac8b6f9c
Julieta Delgado <julidmeza@gmail.com>
2022-04-23 18:06:37 -0500

    Create endpoint to update an explorer

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ app.post('/v1/explorers', (req,res) => {
     res.status(201).json({message: 'Created'})
 })
 
+app.put('/v1/explorers/:id', (req,res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body // Params of a client
+    res.status(200).json({message: 'Updated!'})
+})
+
 // Initial path, this path will respond to the url localhost:3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
