7717a22fc760606497a70f4e9b2fe0093ce1a941
Julieta Delgado <julidmeza@gmail.com>
2022-04-23 18:04:09 -0500

    Create endpoint to create an explorer

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body // Params of a client
+    res.status(201).json({message: 'Created'})
+})
+
 // Initial path, this path will respond to the url localhost:3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
