d5c16b17578d8a1c1daef3e1020f64121e1d2fd0
Julieta Delgado <julidmeza@gmail.com>
2022-04-23 17:52:08 -0500

    Test endpoint

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.use(express.json()) // Indicate that we are going to use json
 // Port in wich we are going to see our app: localhost:3000
 const port = 3000
 
+// HTTP methods
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id: 1, name: 'Juls1'}
+    const explorer2 = {id: 2, name: 'Juls2'}
+    const explorer3 = {id: 3, name: 'Juls3'}
+    const explorer4 = {id: 4, name: 'Juls4'}
+
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+
+})
+
 // Initial path, this path will respond to the url localhost:3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
