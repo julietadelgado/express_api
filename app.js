8a8f1408e57924f6983facc1985e6a3634c4e364
Julieta Delgado <julidmeza@gmail.com>
2022-04-23 17:58:37 -0500

    Create and test endpoint to get user by id

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.get('/v1/explorers', (req, res) => {
 
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: 'Julieta'}
+    res.status(200).json(explorer)
+})
+
 // Initial path, this path will respond to the url localhost:3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
